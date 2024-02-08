06c5e225eca86211348f997508dbb4e871acd7b2
vepifanio <vepifanio1@gmail.com>
2024-02-07 21:54:12 -0300

    Aula 03

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -30,8 +31,27 @@ export async function getPoll(app: FastifyInstance) {
       })
     }
 
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES")
+
+    const votes = result.reduce((obj, line, idx) => {
+      if (idx % 2 === 0) {
+        const score = result[idx + 1]
+        Object.assign(obj, { [line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
     return reply.send({
-      poll
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => ({
+          id: option.id,
+          title: option.title,
+          score: (option.id in votes) ? votes[option.id] : 0
+        }))
+      }
     })
   })
 }
